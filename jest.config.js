149470f8b275a92f879e08c3d93cbcab26ef07ad
st-quangnguyen2 <quang.nguyen2@supremetech.vn>
2023-04-23 22:07:46 +0700

    init

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  testEnvironment: 'jest-environment-jsdom',
+  modulePaths: ['src'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+};
